ec42ba41a6251ddb0db7925c2f475b31633b69e0
Jessica Kerr <jessitron@atomist.com>
2018-05-01 17:29:53 -0500

    implement finding exactly one issue with this title

--- src/blueprint/issue/issueRaisingReviewListener.ts
+++ src/blueprint/issue/issueRaisingReviewListener.ts
@@ -21,7 +21,8 @@ import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitH
 import { logger } from "@atomist/automation-client";
 import { authHeaders } from "@atomist/sdm/util/github/ghub";
 import axios from "axios";
-import { TokenCredentials } from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
+import { ProjectOperationCredentials, TokenCredentials } from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
+import { RemoteRepoRef } from "@atomist/automation-client/operations/common/RepoId";
 
 const CloudReadinessIssueTitle = "Cloud Readiness";
 
@@ -31,13 +32,37 @@ export const IssueRaisingReviewListener: ReviewListener = async (ri: ReviewListe
         return;
     }
     // Find the well-known issue if there is one
-    const existingIssue = findIssues((ri.credentials as TokenCredentials).token, ri.id as GitHubRepoRef, CloudReadinessIssueTitle);
+    const existingIssue = findIssue((ri.credentials as TokenCredentials).token, ri.id as GitHubRepoRef, CloudReadinessIssueTitle);
+    if (!existingIssue) {
+        // create it
+    } else {
+        // replace the body? and set state to open
+    }
 };
 
-function findIssues(token: string,
-                      grr: GitHubRepoRef,
-                      title: string): Promise<Issue> {
-    const url = `${grr.apiBase}/repos/${grr.owner}/${grr.repo}/issues`;
-    logger.debug(`Request to '${url}' to get issues`);
-    return axios.get(url, authHeaders(token)).then(r => r.data);
+interface KnownIssue extends Issue {
+    state: "open" | "closed";
+    number: number;
+}
+
+// find the most recent open (or closed, if none are open) issue with precisely this title
+async function findIssue(credentials: ProjectOperationCredentials,
+                    rr: RemoteRepoRef,
+                    title: string): Promise<KnownIssue> {
+    const token = (credentials as TokenCredentials).token;
+    const grr = rr as GitHubRepoRef;
+    const url = `${grr.apiBase}/search/issues?q=is:issue+user:${rr.owner}+repo:${rr.repo}+"${title}"`;
+    console.log(`Request to '${url}' to get issues`);
+    const returnedIssues: KnownIssue[] = await axios.get(url, authHeaders(token)).then(r => r.data.items);
+    return returnedIssues.sort(openFirst).filter(i => i.title === title)[0];
+}
+
+function openFirst(a: KnownIssue, b: KnownIssue): number {
+    if (a.state === "open" && b.state === "closed") {
+        return -1;
+    }
+    if (b.state === "open" && a.state === "closed") {
+        return 1;
+    }
+    return b.number - a.number; // if same state, most recent one first.
 }
