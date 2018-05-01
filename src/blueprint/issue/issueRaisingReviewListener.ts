8b9e5faf59baa9b341e0334519adf24ba1688880
Jessica Kerr <jessitron@atomist.com>
2018-05-01 18:26:23 -0500

    create, update issue if there are problems

--- src/blueprint/issue/issueRaisingReviewListener.ts
+++ src/blueprint/issue/issueRaisingReviewListener.ts
@@ -28,6 +28,9 @@ import { OnFirstPushToRepo } from "@atomist/sdm";
 import Push = OnFirstPushToRepo.Push;
 import * as slack from "@atomist/slack-messages";
 import * as _ from "lodash";
+import { ReviewComment } from "@atomist/automation-client/operations/review/ReviewResult";
+import * as stringify from "json-stringify-safe";
+
 
 const CloudReadinessIssueTitle = "Cloud Readiness";
 const CloudReadinessReviewCommentCategories = [ImportDotStarCategory];
@@ -37,7 +40,7 @@ export const IssueRaisingReviewListener: ReviewListener = async (ri: ReviewListe
         // We only care about pushers to the default branch
         return;
     }
-    const relevantComments = ri.review.comments.filter( rc => CloudReadinessReviewCommentCategories.includes(rc.category));
+    const relevantComments = ri.review.comments.filter(rc => CloudReadinessReviewCommentCategories.includes(rc.category));
     // Find the well-known issue if there is one
     const existingIssue = await findIssue((ri.credentials as TokenCredentials).token, ri.id as GitHubRepoRef, CloudReadinessIssueTitle);
 
@@ -45,19 +48,37 @@ export const IssueRaisingReviewListener: ReviewListener = async (ri: ReviewListe
         logger.debug("0 of %d comments were relevant", ri.review.comments.length);
         if (existingIssue) {
             // close it
-            logger.info("Closing issue %d because all comments have been addressed");
+            logger.info("Closing issue %d because all comments have been addressed", existingIssue.number);
             const congrats: string = `The last review problem was fixed by ${who(ri.push)} when they pushed ${linkToSha(ri.id)}`;
-            await updateIssue(ri.credentials, ri.id, { ...existingIssue, state: "closed", body: congrats});
+            await updateIssue(ri.credentials, ri.id, {...existingIssue, state: "closed", body: congrats});
         }
         return;
     }
 
+    // there are some comments
     if (!existingIssue) {
-        // create it
+        logger.info("Creating issue '%s' from review comments", CloudReadinessIssueTitle);
+        await createIssue(ri.credentials, ri.id,
+            {
+                title: CloudReadinessIssueTitle,
+                body: bodyFromComments(relevantComments),
+                // labels? assignees?
+            });
     } else {
-        // replace the body? and set state to open
+        logger.info("Updating issue %d with the latest comments", existingIssue.number);
+        await updateIssue(ri.credentials, ri.id,
+            {
+                ...existingIssue,
+                state: "open",
+                body: bodyFromComments(relevantComments)
+            });
     }
+    // Should we catch exceptions and not fail the Goal if this doesn't work?
 };
+
+function bodyFromComments(ri: ReviewComment[]): string {
+    return stringify(ri);
+}
 
 function who(push: Push) {
     const screenName: string = _.get(push, "after.committer.person.chatId.screenName");
@@ -82,15 +103,24 @@ async function updateIssue(credentials: ProjectOperationCredentials,
     const token = (credentials as TokenCredentials).token;
     const grr = rr as GitHubRepoRef;
     const url = `${grr.apiBase}/repos/${rr.owner}/${rr.repo}/issues/${issue.number}`;
-    console.log(`Request to '${url}' to get issues`);
-    const returnedIssues: KnownIssue[] = await axios.patch(url, authHeaders(token)).then(r => r.data.items);
-    return returnedIssues.filter(i => i.title === title).sort(openFirst)[0];
+    console.log(`Request to '${url}' to update issue`);
+    await axios.patch(url, issue, authHeaders(token));
+}
+
+async function createIssue(credentials: ProjectOperationCredentials,
+                           rr: RemoteRepoRef,
+                           issue: Issue) {
+    const token = (credentials as TokenCredentials).token;
+    const grr = rr as GitHubRepoRef;
+    const url = `${grr.apiBase}/repos/${rr.owner}/${rr.repo}/issues`;
+    console.log(`Request to '${url}' to create issue`);
+    await axios.put(url, issue, authHeaders(token));
 }
 
 // find the most recent open (or closed, if none are open) issue with precisely this title
 async function findIssue(credentials: ProjectOperationCredentials,
-                    rr: RemoteRepoRef,
-                    title: string): Promise<KnownIssue> {
+                         rr: RemoteRepoRef,
+                         title: string): Promise<KnownIssue> {
     const token = (credentials as TokenCredentials).token;
     const grr = rr as GitHubRepoRef;
     const url = `${grr.apiBase}/search/issues?q=is:issue+user:${rr.owner}+repo:${rr.repo}+"${title}"`;
