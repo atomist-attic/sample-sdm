b01f4f582e5349e55447765a4062d7ac629b8b4e
Rod Johnson <rod@atomist.com>
2018-09-22 17:42:08 +1000

    Polishing

--- src/feature/support/Features.ts
+++ src/feature/support/Features.ts
@@ -29,7 +29,7 @@ import {
     buttonForCommand,
     Fingerprint,
     logger,
-    RemoteRepoRef,
+    RemoteRepoRef, RepoRef,
 } from "@atomist/automation-client";
 import { FeatureStore } from "../FeatureStore";
 import {
@@ -60,17 +60,20 @@ export class Features implements Enabler {
             .forEach(f => enableFeature(sdm, this.store, f, goals));
     }
 
-    private listFeaturesCommand(): CodeInspectionRegistration<string[]> {
+    private listFeaturesCommand(): CodeInspectionRegistration<Fingerprint[]> {
         return {
             name: "feature-list",
             intent: "list features",
             inspection: async p => {
-                const scans = await Promise.all(this.features.map(f => f.projectFingerprinter(p)));
-                return scans.map(s => s.name).sort();
+                const scans = await Promise.all(
+                    this.features.map(f => f.projectFingerprinter(p)));
+                return scans
+                    .filter(s => !!s)
+                    .sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
             },
             onInspectionResults: async (results, ci) => {
                 for (const r of results) {
-                    await ci.addressChannels(`Repo ${r.repoId.url}: \`${results.map(r => r.result)}\``)
+                    await ci.addressChannels(`Repo ${r.repoId.url}: \`${results.map(r => r.result.map(s => s.name))}\``);
                 }
             },
         };
@@ -115,8 +118,8 @@ function enableFeature(sdm: SoftwareDeliveryMachine,
 }
 
 /**
- * Listen to pushes of relevant projects and roll out upgrades
- * to relevant downstream projects
+ * Listen to pushes to projects with this feature and roll out upgrades
+ * to relevant downstream projects if the relevant version moves the ideal
  * @param {SoftwareDeliveryMachine} sdm
  * @param {FeatureStore} store
  * @param {Feature} f
@@ -138,9 +141,9 @@ function listenAndRolloutUpgrades(sdm: SoftwareDeliveryMachine,
                 if (!!ideal) {
                     const after = await f.projectFingerprinter(pu.project);
                     if (f.compare(ideal, after, ComparisonPolicy.quality) > 0) {
-                        // TODO ask
+                        // TODO ask about setting ideal
                         await store.setIdeal(after);
-                        return rolloutToDownstreamProjects(f, ideal, transformName, sdm, pu);
+                        return rolloutQualityOrderedFeatureToDownstreamProjects(f, ideal, transformName, sdm, pu);
                     } else {
                         logger.info("Ideal feature %s value is %j, ours is %j and it's fine", f.name, ideal, after);
                     }
@@ -156,32 +159,51 @@ function listenAndRolloutUpgrades(sdm: SoftwareDeliveryMachine,
  * Roll out buttons in all repos to apply this version of the feature
  * @return {Promise<void>}
  */
-async function rolloutToDownstreamProjects<S extends Fingerprint>(feature: Feature<S>,
-                                                                  valueToUpgradeTo: S,
-                                                                  command: string,
-                                                                  sdm: SoftwareDeliveryMachine,
-                                                                  i: PushImpactListenerInvocation) {
+async function rolloutQualityOrderedFeatureToDownstreamProjects<S extends Fingerprint>(feature: Feature<S>,
+                                                                                       valueToUpgradeTo: S,
+                                                                                       command: string,
+                                                                                       sdm: SoftwareDeliveryMachine,
+                                                                                       i: PushImpactListenerInvocation) {
     logger.info("Rolling out command '%s' to apply feature %s", command, feature.name);
     return doWithRepos(sdm, i,
         async p => {
-            const found = await feature.projectFingerprinter(p);
-            // Only upgrade if they have a lower level of this feature
-            if (!!found && feature.compare(found, valueToUpgradeTo, ComparisonPolicy.quality) < 0) {
-                const attachment: Attachment = {
-                    text: `Accept new feature *${feature.name}*: ${feature.summary(valueToUpgradeTo)}?`,
-                    fallback: "accept feature",
-                    actions: [buttonForCommand({ text: `Accept feature ${feature.name}?` },
-                        command,
-                        { "targets.owner": p.id.owner, "targets.repo": p.id.repo },
-                    ),
-                    ],
-                };
-                const message: SlackMessage = {
-                    attachments: [attachment],
-                };
-                await i.context.messageClient.addressChannels(message, p.id.repo);
+            const existingValue = await feature.projectFingerprinter(p);
+            // Only upgrade if the project have a lower level of this feature
+            if (!!existingValue && feature.compare(existingValue, valueToUpgradeTo, ComparisonPolicy.quality) < 0) {
+                await offerFeatureToProject(feature, existingValue, valueToUpgradeTo, command, p.id, i);
             }
         });
+}
+
+/**
+ * Offer the given feature value to the project with the given id
+ * @param {Feature<S extends Fingerprint>} feature
+ * @param existingValue value of the feature currently in this project
+ * @param {S} valueToUpgradeTo
+ * @param {string} command
+ * @param {RepoRef} id
+ * @param {SdmContext} i
+ * @return {Promise<void>}
+ */
+async function offerFeatureToProject<S extends Fingerprint>(feature: Feature<S>,
+                                                            existingValue: S,
+                                                            valueToUpgradeTo: S,
+                                                            command: string,
+                                                            id: RepoRef,
+                                                            i: SdmContext) {
+    const attachment: Attachment = {
+        text: `Accept new feature *${feature.name}*: ${feature.summary(valueToUpgradeTo)} vs existing ${feature.summary(existingValue)}?`,
+        fallback: "accept feature",
+        actions: [buttonForCommand({ text: `Accept feature ${feature.name}?` },
+            command,
+            { "targets.owner": id.owner, "targets.repo": id.repo },
+        ),
+        ],
+    };
+    const message: SlackMessage = {
+        attachments: [attachment],
+    };
+    await i.context.messageClient.addressChannels(message, id.repo);
 }
 
 /**
@@ -195,9 +217,13 @@ async function doWithRepos(sdm: SoftwareDeliveryMachine,
                            i: SdmContext,
                            action: WithLoadedProject<any>): Promise<any> {
     const repos = await sdm.configuration.sdm.repoFinder(i.context);
+    logger.info("doWithRepos working on %d repos", repos.length);
     await Promise.all(repos.map(id => {
         return sdm.configuration.sdm.projectLoader.doWithProject(
             { credentials: i.credentials, id: id as RemoteRepoRef, readOnly: true },
-            action);
+            action)
+            .catch(err => {
+                logger.warn("Project err: %s", err);
+            });
     }));
 }
